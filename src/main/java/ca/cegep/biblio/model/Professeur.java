46676f630417c9c0eab4c2b1eb7f5b3f108705db
Eduardo De Oliveira Rodrigues <eduoliveira4489@gmail.com>
2026-05-26 19:32:25 -0400

    Classe Professeur.java au complet

--- src/main/java/ca/cegep/biblio/model/Professeur.java
+++ src/main/java/ca/cegep/biblio/model/Professeur.java
@@ -1,5 +1,28 @@
 package ca.cegep.biblio.model;
 
-public class Professeur {
+public class Professeur extends Usager {
 
+    // Constructeur
+    public Professeur(String nom, String id) {
+        super(nom, id);
+    }
+
+    @Override
+    public int getMaxLivres() {
+        return 6;
+    }
+
+    @Override
+    public int getDureeEmpruntJours() {
+        return 30;
+    }
+
+    @Override
+    public String toString() {
+        return "Professeur{" +
+                "nom='" + getNom() + '\'' +
+                ", id='" + getId() + '\'' +
+                ", empruntsEnCours=" + getEmpruntsEnCours().size() +
+                '}';
+    }
 }
