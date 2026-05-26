9e559f994a3217ec2d244a7e483233e0ec41b7c7
Eduardo De Oliveira Rodrigues <eduoliveira4489@gmail.com>
2026-05-26 19:33:54 -0400

    Classe Visiteur.java au complet

--- src/main/java/ca/cegep/biblio/model/Visiteur.java
+++ src/main/java/ca/cegep/biblio/model/Visiteur.java
@@ -1,5 +1,28 @@
 package ca.cegep.biblio.model;
 
-public class Visiteur  {
+public class Visiteur extends Usager {
 
+    // Constructeur
+    public Visiteur(String nom, String id) {
+        super(nom, id);
+    }
+
+    @Override
+    public int getMaxLivres() {
+        return 1;
+    }
+
+    @Override
+    public int getDureeEmpruntJours() {
+        return 7;
+    }
+
+    @Override
+    public String toString() {
+        return "Visiteur{" +
+                "nom='" + getNom() + '\'' +
+                ", id='" + getId() + '\'' +
+                ", empruntsEnCours=" + getEmpruntsEnCours().size() +
+                '}';
+    }
 }
