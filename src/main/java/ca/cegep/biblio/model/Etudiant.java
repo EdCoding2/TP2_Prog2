759835f19e31573610f279304950dc8d08fce2d8
Eduardo De Oliveira Rodrigues <eduoliveira4489@gmail.com>
2026-05-26 19:31:39 -0400

    Classe Etudiant.java au complet

--- src/main/java/ca/cegep/biblio/model/Etudiant.java
+++ src/main/java/ca/cegep/biblio/model/Etudiant.java
@@ -1,5 +1,28 @@
 package ca.cegep.biblio.model;
 
-public class Etudiant {
+public class Etudiant extends Usager {
 
+    // Constructeur
+    public Etudiant(String nom, String id) {
+        super(nom, id);
+    }
+
+    @Override
+    public int getMaxLivres() {
+        return 3;
+    }
+
+    @Override
+    public int getDureeEmpruntJours() {
+        return 14;
+    }
+
+    @Override
+    public String toString() {
+        return "Etudiant{" +
+                "nom='" + getNom() + '\'' +
+                ", id='" + getId() + '\'' +
+                ", empruntsEnCours=" + getEmpruntsEnCours().size() +
+                '}';
+    }
 }
